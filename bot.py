ee39c04f34063bf9f6e7b3e7fcb10a6ebb99c483
tomosev <tomosev@pm.me>
2020-12-03 01:01:01 +0000

    update

--- bot.py
+++ bot.py
@@ -1,42 +1,33 @@
 import discord
+from discord.ext import commands
 from data import webCollectData
 from dotenv import load_dotenv
 import os
-import json
+import datetime
+bot = discord.Client()
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.command()
+async def schedule(ctx):
+    string = ""
+    nl = '\n'
+    time = datetime.datetime.now().year
+    data = webCollectData().apiRaceSchedule()
+    for race in data["MRData"]["RaceTable"]["Races"]:
+        name = race["raceName"]
+        circuit = race["Circuit"]["circuitName"]
+        string += f"**{name}**: {circuit} {nl}{nl}"
+    embed = discord.Embed(title=f"Race Schedule {time}",
+                          description=string, color=0xe60000)
+    await ctx.send(embed=embed)
+
+
+@ bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
 
 load_dotenv()
-
 KEY = os.environ.get("DISCORD_KEY")
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-
-# class myclient(discord.Client): 
-#     async def on_ready(self):
-#         data = webCollectData().apiRoundData()
-#         print(data['MRData'] ['RaceTable'] ['season']) 
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!race schedule'):
-        data = webCollectData().apiRaceSchedule()
-        for race in data["MRData"] ["RaceTable"] ["Races"]:
-            name = race["raceName"]
-            circuit = race["Circuit"] ["circuitName"]
-            messagecontent = name, circuit
-            await message.channel.send(messagecontent)
-
-        
-
-
-
-client.run(KEY)
-
+bot.run(KEY)
