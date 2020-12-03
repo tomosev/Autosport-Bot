42caba02c3c2a513eda814eae83913cb8ccefa09
tomosev <tomosev@pm.me>
2020-12-03 19:24:59 +0000

    new updates

--- bot.py
+++ bot.py
@@ -11,7 +11,11 @@ import datetime
 
 bot = discord.Client()
 bot = commands.Bot(command_prefix="!")
+# embed re-used fields
 bot_name = "Motorsport Bot"
+flagemoji = ":checkered_flag:"
+nl = '\n'
+embed = discord.Embed()
 
 
 @bot.event
@@ -20,16 +24,14 @@ async def on_ready():
 
 
 @bot.command()
-async def schedule(ctx):
+async def calandar(ctx):
     fields = []
-    nl = '\n'
-    time = datetime.datetime.now().year
+    year = datetime.datetime.now().year
     data = webCollectData().apiRaceSchedule()
-    flagemoji = ":checkered_flag:"
     for race in data["MRData"]["RaceTable"]["Races"]:
         name = race["raceName"]
         circuit = race["Circuit"]["circuitName"]
-        url = race["url"]
+        #url = race["url"]
         unformatteddate = race["date"]
         unformattedtime = race["time"]
         time = unformattedtime[0:5]
@@ -37,17 +39,34 @@ async def schedule(ctx):
             unformatteddate, "%Y-%m-%d").strftime("%d/%m/%Y")
         fields.append([
             f"{flagemoji}**{name}**",
-            f"Date: {date} {nl} Time: {time}(zulutime) {nl} Location: {circuit}"
+            f"Date: {date}{nl}Time: {time} (zulutime) {nl}Location: {circuit}"
         ])
     embed = EmbedWithFields(
-        title=f"Race Schedule {time}",
+        title=f"Race Schedule {year}",
         color=0xe60000,
         description="",
         fields=fields,
-
     )
     embed.set_footer(text=bot_name)
     await ctx.send(embed=embed)
+
+
+@bot.command()
+async def driver(ctx, arg):
+    data = webCollectData().apiDriverInfo(name=arg)
+    for race in data["MRData"]["DriverTable"]["Drivers"]:
+        first_name = race["givenName"]
+        last_name = race["familyName"]
+        number = race["permanentNumber"]
+        code = race["code"]
+        dob = race["dateOfBirth"]
+        nation = race["nationality"]
+        url = race["url"]
+        embed = discord.Embed(title=f"{first_name} {last_name}",
+                              description=f"Number: {number}{nl}Code: {code}{nl} DOB: {dob}{nl} Nationality: {nation}{nl} {url}")
+        # embed.add_field(name=f"{first_name} {last_name}",
+        #                 value=f"Number: {number}{nl}Code: {code}{nl} DOB: {dob}{nl} Nationality: {nation}{nl} {url}", inline=True)
+        await ctx.send(embed=embed)
 
 
 load_dotenv()
--- data.py
+++ data.py
@@ -27,37 +27,43 @@ class webCollectData:
         self.json_data = json.loads(script)
         return self.json_data
 
-    def apiRoundData(self):
-        api_url = (
-            f"http://ergast.com/api/f1/{roundParam['year']}/{roundParam['round']}.json"
-        )
-        r = requests.get(api_url)
-        self.json_data = r.json()
-        return self.json_data
-
     def apiRaceSchedule(self):
         api_url = "http://ergast.com/api/f1/current.json"
         r = requests.get(api_url)
         self.json_data = r.json()
         return self.json_data
 
-    def apiRaceResults(self):
-        api_url = "http://ergast.com/api/f1/current/last/results.json"
+    def apiDriverInfo(self, name):
+        api_url = f"http://ergast.com/api/f1/drivers/{name}.json"
         r = requests.get(api_url)
         self.json_data = r.json()
         return self.json_data
 
-    def apiFastestLap(self):
-        api_url = f"http://ergast.com/api/f1/{roundParam['year']}/{roundParam['round']}/fastest/1/results.json"
-        r = requests.get(api_url)
-        self.json_data = r.json()
-        return self.json_data
+    # def apiRaceResults(self):
+    #     api_url = "http://ergast.com/api/f1/current/last/results.json"
+    #     r = requests.get(api_url)
+    #     self.json_data = r.json()
+    #     return self.json_data
 
-    def apiQualiResults(self):
-        api_url = f"http://ergast.com/api/f1/{roundParam['year']}/{roundParam['round']}/qualifying.json"
-        r = requests.get(api_url)
-        self.json_data = r.json()
-        return self.json_data
+    # def apiFastestLap(self):
+    #     api_url = f"http://ergast.com/api/f1/{roundParam['year']}/{roundParam['round']}/fastest/1/results.json"
+    #     r = requests.get(api_url)
+    #     self.json_data = r.json()
+    #     return self.json_data
+
+    # def apiQualiResults(self):
+    #     api_url = f"http://ergast.com/api/f1/{roundParam['year']}/{roundParam['round']}/qualifying.json"
+    #     r = requests.get(api_url)
+    #     self.json_data = r.json()
+    #     return self.json_data
+
+    # def apiRoundData(self):
+    #     api_url = (
+    #         f"http://ergast.com/api/f1/{roundParam['year']}/{roundParam['round']}.json"
+    #     )
+    #     r = requests.get(api_url)
+    #     self.json_data = r.json()
+    #     return self.json_data
 
 
 # Import time to add sleep functionality to stop mass requests
