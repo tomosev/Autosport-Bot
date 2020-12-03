f857762fc47b5dc308d26f39310ce06ed021371d
tomosev <tomosev@pm.me>
2020-12-03 22:14:27 +0000

    added new commands

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ bot = commands.Bot(command_prefix="!")
 # embed re-used fields
 bot_name = "Motorsport Bot"
 flagemoji = ":checkered_flag:"
+caremoji = ":race_car:"
 nl = '\n'
 embed = discord.Embed()
 
@@ -24,7 +25,11 @@ async def on_ready():
     print('Green flag: {0.user}'.format(bot))
 
 
-@bot.command()
+# Formula one commands
+
+
+@bot.command(name="f1calandar")
+# gets all races for a season
 async def calandar(ctx):
     fields = []
     year = datetime.datetime.now().year
@@ -43,16 +48,48 @@ async def calandar(ctx):
             f"Date: {date}{nl}Time: {time} (zulutime) {nl}Location: {circuit}  [{name} on Wikipedia]({url})"
         ])
     embed = EmbedWithFields(
-        title=f"Race Schedule {year}",
+        title=f"Formula 1 Race Calandar {year}",
         color=0xe60000,
-        description="",
+        description=f"The current {year} Formula 1 calandar",
+        fields=fields,
+    )
+    embed.set_footer(text=bot_name)
+    await ctx.send(embed=embed)
+
+# race results last race
+# http://ergast.com/api/f1/current/last/results
+
+# season results
+# http://ergast.com/api/f1/2008/results/1
+
+
+@bot.command(name="f1drivers")
+# gets all formulaone drivers
+async def drivers(ctx):
+    fields = []
+    year = datetime.datetime.now().year
+    data = webCollectData().apiDriversAll(year)
+    for race in data["MRData"]["DriverTable"]["Drivers"]:
+        first_name = race["givenName"]
+        last_name = race["familyName"]
+        number = race["permanentNumber"]
+        code = race["code"]
+        fields.append([
+            f"{caremoji}**{first_name} {last_name}**",
+            f"Number: {number}{nl}Code: {code}"
+        ])
+    embed = EmbedWithFields(
+        title=f"Formula 1 Driver List {year}",
+        color=0xe60000,
+        description=f"The current {year} Formula 1 driver list",
         fields=fields,
     )
     embed.set_footer(text=bot_name)
     await ctx.send(embed=embed)
 
 
-@bot.command()
+@bot.command(name="f1driver")
+# gets specific driver information
 async def driver(ctx, arg):
     data = webCollectData().apiDriverInfo(name=arg)
     for race in data["MRData"]["DriverTable"]["Drivers"]:
@@ -60,11 +97,13 @@ async def driver(ctx, arg):
         last_name = race["familyName"]
         number = race["permanentNumber"]
         code = race["code"]
-        dob = race["dateOfBirth"]
+        unformated_dob = race["dateOfBirth"]
         nation = race["nationality"]
         url = race["url"]
-        embed = discord.Embed(title=f"{flagemoji}{first_name} {last_name}",
-                              description=f"**Number:** {number}{nl}**Code:** {code}{nl}**DOB**: {dob}{nl}**Nationality:** {nation}{nl}  [{first_name} {last_name} on Wikipedia]({url})")
+        dateofbirth = datetime.datetime.strptime(
+            unformated_dob, "%Y-%m-%d").strftime("%d/%m/%Y")
+        embed = discord.Embed(title=f"{caremoji}{first_name} {last_name}",
+                              description=f"**Number:** {number}{nl}**Code:** {code}{nl}**DOB**: {dateofbirth}{nl}**Nationality:** {nation}{nl}  [{first_name} {last_name} on Wikipedia]({url})", color=0xe60000)
         image = last_name.lower()
         if image in driver_images:
             embed.set_thumbnail(url=driver_images[image])
@@ -72,7 +111,32 @@ async def driver(ctx, arg):
         await ctx.send(embed=embed)
 
 
-@ bot.command(name="team")
+@bot.command(name="f1teams")
+# gets all formula one constructors
+async def constructors(ctx):
+    fields = []
+    year = datetime.datetime.now().year
+    data = webCollectData().apiConstructorAll(year)
+    for team in data["MRData"]["ConstructorTable"]["Constructors"]:
+        name = team["name"]
+        nation = team["nationality"]
+        url = team["url"]
+        fields.append([
+            f"{caremoji}**{name}**",
+            f"Origin: {nation}{nl}[More info]({url})"
+        ])
+    embed = EmbedWithFields(
+        title=f"Formula 1 Team List {year}",
+        color=0xe60000,
+        description=f"The current {year} Formula 1 team list",
+        fields=fields,
+    )
+    embed.set_footer(text=bot_name)
+    await ctx.send(embed=embed)
+
+
+@ bot.command(name="f1team")
+# gets specific constructor information
 async def constructor(ctx, arg):
     data = webCollectData().apiConstructorInfo(name=arg)
     for team in data["MRData"]["ConstructorTable"]["Constructors"]:
--- data.py
+++ data.py
@@ -19,13 +19,13 @@ load_dotenv()
 
 
 class webCollectData:
-    def webScrapeData(self):
-        web_url = os.environ.get("WEB_URL")
-        r = requests.get(web_url)
-        soup = BeautifulSoup(r.content, "html.parser")
-        script = soup.find_all("script")[6].string.strip()[11:]
-        self.json_data = json.loads(script)
-        return self.json_data
+    # def webScrapeData(self):
+    #     web_url = os.environ.get("WEB_URL")
+    #     r = requests.get(web_url)
+    #     soup = BeautifulSoup(r.content, "html.parser")
+    #     script = soup.find_all("script")[6].string.strip()[11:]
+    #     self.json_data = json.loads(script)
+    #     return self.json_data
 
     def apiRaceSchedule(self):
         api_url = "http://ergast.com/api/f1/current.json"
@@ -39,37 +39,23 @@ class webCollectData:
         self.json_data = r.json()
         return self.json_data
 
+    def apiDriversAll(self, year):
+        api_url = f"http://ergast.com/api/f1/{year}/drivers/.json"
+        r = requests.get(api_url)
+        self.json_data = r.json()
+        return self.json_data
+
     def apiConstructorInfo(self, name):
         api_url = f"http://ergast.com/api/f1/constructors/{name}.json"
         r = requests.get(api_url)
         self.json_data = r.json()
         return self.json_data
 
-    # def apiRaceResults(self):
-    #     api_url = "http://ergast.com/api/f1/current/last/results.json"
-    #     r = requests.get(api_url)
-    #     self.json_data = r.json()
-    #     return self.json_data
-
-    # def apiFastestLap(self):
-    #     api_url = f"http://ergast.com/api/f1/{roundParam['year']}/{roundParam['round']}/fastest/1/results.json"
-    #     r = requests.get(api_url)
-    #     self.json_data = r.json()
-    #     return self.json_data
-
-    # def apiQualiResults(self):
-    #     api_url = f"http://ergast.com/api/f1/{roundParam['year']}/{roundParam['round']}/qualifying.json"
-    #     r = requests.get(api_url)
-    #     self.json_data = r.json()
-    #     return self.json_data
-
-    # def apiRoundData(self):
-    #     api_url = (
-    #         f"http://ergast.com/api/f1/{roundParam['year']}/{roundParam['round']}.json"
-    #     )
-    #     r = requests.get(api_url)
-    #     self.json_data = r.json()
-    #     return self.json_data
+    def apiConstructorAll(self, year):
+        api_url = f"http://ergast.com/api/f1/{year}/constructors.json"
+        r = requests.get(api_url)
+        self.json_data = r.json()
+        return self.json_data
 
 
 # Import time to add sleep functionality to stop mass requests
